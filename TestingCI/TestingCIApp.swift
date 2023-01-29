70c47d19deea6c43f9e30cbdc48828e0d1a48fd6
Riccardo Cipolleschi <cipolleschi@fb.com>
2023-01-29 16:44:01 +0000

    Initial Commit

---
+++ TestingCI/TestingCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCIApp.swift
+//  TestingCI
+//
+//  Created by Riccardo Cipolleschi on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
